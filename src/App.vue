6a690ea30a87cba431bd088855ba3903e42aa50d
钟金辰 <2541842721@qq.com>
2024-05-22 13:43:31 +0800

    配置

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
 import Downloader from "./components/Downloader.vue";
-import { reactive, ref } from "vue";
 
-const globalData = reactive({ value: "http://shipin.cdxyhpx.com/sv/3bba3a21-18926843f74/3bba3a21-18926843f74.mp4" });
-const setGlobalData = (obj: any) => {
-  globalData.value = obj.value;
-}
-
-const url = ref("");
-const actionflow_id = ref("39b8c22f-5d78-446b-a046-a2967986da5c")
 const writeXlsxFileConfig = '{"objects":[{"content":"zion中测试"}],"schema":[{"column":"测试标题","type":"String","value":"item => item.content"}]}';
 
 </script>
